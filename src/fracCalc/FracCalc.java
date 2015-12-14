6e35d95cc6fd0869bfd4befaa185add0d2d656a7
Jimin Lee <jimin3422@gmail.com>
2015-12-13 23:52:08 -0500

    COMPLETE

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -1,184 +1,143 @@
 package fracCalc;
 
-import java.util.Scanner;
-
+import java.util.*;
 public class FracCalc {
 
-//	public String produceAnswer(String input)
-//	{
-//			
-//			for (int n=0; n<= input.length() ; n++)
-//			{
-//				if (input.indexOf(n) = "+" || "-" || "*" || "/")
-//				
-//				{
-//					String operator = input.substring((n, n+1));
-//					
-//				}
-//			}
-//			String firstOperand = input.substring(substring (0, input.indexOf(operator));
-//			String secondOperand = input.substring(input.indexOf(operator)+1);
-//			
-//			return secondOperand;
-//		}
-		
-		public static void main(String []args)
-		{
-			Scanner a = new Scanner(System.in);
-			System.out.println("Input an arithmetic equation ); ");
-			
-			String A = a.nextLine();
-			while(!A.equalsIgnoreCase("quit"))
-			
-			{
-				System.out.println(produceAnswer(A));
-				System.out.println("Input another equation:");
-				A = a.nextLine();
-			}
-			
-			System.out.println("Thank you.");
-		}
-
-    public static String produceAnswer(String input)
-    { 
-        // TODO: Implement this function to produce the solution to the input
-    	
-    	String operator = "";
-    	int operator_position = 0;
-    	int int1 = 0;
-    	int num1 = 0;
-    	int den1 = 1;
-    	int int2 = 0;
-    	int num2 = 0;
-    	int den2 = 1;
-       
-    	//System.out.println(input);
-    	//System.out.println(input.length());
-    	
-    	//System.out.println(input.substring(1));
-    	
-    	for (int n=0; n<input.length() ; n++)
-		{
- //   		System.out.println("n = " + n);
-//    		System.out.println(input.substring(n,n+1));
-    		String test = input.substring(n,n +1);
-			if (test.equals(" ") && operator.equals(""))
-			
-			{
-				operator = input.substring(n+1, n+2);
-				operator_position = n+1; 
-			
-				
-	//		System.out.println(operator);
-			}
-		}
-    	
-		String firstOperand = input.substring(0, input.indexOf(operator));
-		String secondOperand = input.substring(operator_position+2);
-		
-		//return secondOperand;
+    public static void main(String []args)
+    {
+        Scanner a = new Scanner(System.in);
+        System.out.println("Input an arithmetic equation: ");
+        String A = a.nextLine();
+        while(!A.equalsIgnoreCase("quit"))
+        {
+            System.out.println(produceAnswer(A));
+            System.out.println("Input another equation: ");
+            A = a.nextLine();
+        }
+        System.out.println("Thank you.");
+    }
     
-    	if(firstOperand.indexOf("_")==-1&&firstOperand.indexOf("/")==-1)
-    	{
-    		int1 = Integer.parseInt(firstOperand);
-    		den1 = 1;
-    	}
-    	else if(firstOperand.indexOf("_")==-1&&firstOperand.indexOf("/")!=-1)
-    	{
-    		
-    		num1 = Integer.parseInt(firstOperand.substring(0, firstOperand.indexOf("/")));
-    		den1 = Integer.parseInt(firstOperand.substring(firstOperand.indexOf("/")+1));
-    	}
-    	else
-    	{
-    		int1 = Integer.parseInt(firstOperand.substring(0,firstOperand.indexOf("_")));
-    		num1 = Integer.parseInt(firstOperand.substring(firstOperand.indexOf("_")+1, firstOperand.indexOf("/")));
-    		den1 = Integer.parseInt(firstOperand.substring(firstOperand.indexOf("/")+1));
-    	}
+    public static String produceAnswer(String input)
+    {
+        String operator = "";
+        int operator_position = 0;
+        int int1 = 0, num1 = 0, den1 = 0, int2 = 0, num2 = 0, den2 = 0;
+        
+        for(int n=0; n<input.length(); n++)
+        {
+            String test = input.substring(n,n+1);
+            if(test.equals(" ")&&operator.equals(""))
+            {
+                operator = input.substring(n+1,n+2);
+                operator_position = n+1;
+            }
+        }
+        
+        String firstOperand = input.substring(0, input.indexOf(operator, input.indexOf("/")+1)-1);
+        String secondOperand = input.substring(operator_position+2);
+        
+        if(firstOperand.indexOf("_")==-1&&firstOperand.indexOf("/")==-1)
+        {
+            int1 = Integer.parseInt(firstOperand);
+            den1 = 1;
+        }
+        else if(firstOperand.indexOf("_")==-1&&firstOperand.indexOf("/")!=-1)
+        {
+            num1 = Integer.parseInt(firstOperand.substring(0, firstOperand.indexOf("/")));
+            den1 = Integer.parseInt(firstOperand.substring(firstOperand.indexOf("/")+1));
+        }
+        else
+        {
+           int1 = Integer.parseInt(firstOperand.substring(0,firstOperand.indexOf("_")));
+           num1 = Integer.parseInt(firstOperand.substring(firstOperand.indexOf("_")+1, firstOperand.indexOf("/")));
+           den1 = Integer.parseInt(firstOperand.substring(firstOperand.indexOf("/")+1));
+        }
 
-    	if(secondOperand.indexOf("_")==-1&&secondOperand.indexOf("/")==-1)
-    	{
-    		int2 = Integer.parseInt(secondOperand);
-    		den2 = 1;
-    	}
-    	
-    	else 
-    	if(secondOperand.indexOf("_")==-1&&secondOperand.indexOf("/")!=-1)
-    	{
-    		num2 = Integer.parseInt(secondOperand.substring(0,secondOperand.indexOf("/")));
-    		den2 = Integer.parseInt(secondOperand.substring(secondOperand.indexOf("/")+1));
-    	}	
-    	else
-    	{
-    		int2 = Integer.parseInt(secondOperand.substring(0,secondOperand.indexOf("_")));
-    		num2 = Integer.parseInt(secondOperand.substring(secondOperand.indexOf("_")+1, secondOperand.indexOf("/")));
-    		den2 = Integer.parseInt(secondOperand.substring(secondOperand.indexOf("/")+1));
-    	}
-
-    	if (operator.equals("+"))
-    	{
-    		int whole = int1 +int2;
-    		if (num1 ==0 && num2 ==0)
-    		{
-    			return "Result:" +whole;
-    		}
-    		else
-    		{ 
-    			int num= 0, den= 0;
-    			if (num1 == 0&& num1 != 0)
-    			{ 
-    				num= num1;
-    				den= den1;
-    				
-    			}
-    			else
-    			{
-    				num = num1*den2*den1;
-    				den = den1 *den2;
-    			}
-    			int gcd = GCD (num,den);
-    			
-    			if (num>den)
-    			{
-    				num = num-den;
-    				whole ++;
-    			}
-    			num = num/gcd;
-    			den = den/gcd;
-    			
-    			if (whole==0)
-    			{
-    				return "Result;"+num+"/"+den;
-    			}
-    			else 
-    			{
-    				return "Result: "+whole+"_"+num+"/"+den;
-    			}
-    		}
-    	}
-    	else if (operator.equals("-"))
-    	{
-    		int whole = int1-int2;
-    		
-    		if(num1==0&& num2==0)
-    		{
-    			return "Result:"+whole;
-    		}
-    		else
-    		{
-    			int num =0, den =0;
-    			if (num1 ==0&& num2!=0)
-    			{
-    				num = num2;
-    				den = den2;
-    			}
-    			else if(num1!=0 && num2 ==0)
-    			{ 
-    				num = num1;
-    				den = den1;
-    			}
-    			else if (num1 ==0 && num2 ==0)
-    			{ num = 0;
+        if(secondOperand.indexOf("_")==-1&&secondOperand.indexOf("/")==-1)
+        {
+            int2 = Integer.parseInt(secondOperand);
+            den2 = 1;
+        }
+        else if(secondOperand.indexOf("_")==-1&&secondOperand.indexOf("/")!=-1)
+        {
+            num2 = Integer.parseInt(secondOperand.substring(0, secondOperand.indexOf("/")));
+            den2 = Integer.parseInt(secondOperand.substring(secondOperand.indexOf("/")+1));
+        }
+        else
+        {
+            int2 = Integer.parseInt(secondOperand.substring(0,secondOperand.indexOf("_")));
+            num2 = Integer.parseInt(secondOperand.substring(secondOperand.indexOf("_")+1, secondOperand.indexOf("/")));
+            den2 = Integer.parseInt(secondOperand.substring(secondOperand.indexOf("/")+1));
+        }
+        
+        if(operator.equals("+"))
+        {
+            int whole = int1+int2;
+            if(num1==0 && num2==0)
+            {
+                return "Result: "+whole;
+            }
+            else
+            {
+                int num = 0, den = 0;
+                if(num1==0 && num1!=0)
+                {
+                    num = num2;
+                    den = den2;
+                }
+                else if(num1!=0 && num2==0)
+                {
+                    num = num1;
+                    den = den1;
+                }
+                else
+                {
+                    num = num1*den2+num2*den1;
+                    den = den1*den2;
+                }
+                int gcd = GCD(num, den);
+                
+                if(num>den)
+                {
+                    num = num-den;
+                    whole++;
+                }
+                num = num/gcd;
+                den = den/gcd;
+                
+                if(whole==0)
+                {
+                    return "Result: "+num+"/"+den;
+                }
+                else
+                {
+                    return "Result: "+whole+"_"+num+"/"+den;
+                }
+            }
+        }
+        else if(operator.equals("-"))
+        {
+            int whole = int1-int2;
+            if(num1==0 && num2==0)
+            {
+                return "Result: "+whole;
+            }
+            else
+            {
+                int num = 0, den = 0;
+                if(num1==0 && num2!=0)
+                {
+                    num = num2;
+                    den = den2;
+                }
+                else if(num1!=0 && num2==0)
+                {
+                    num = num1;
+                    den = den1;
+                }
+                else if(num1==0 && num2==0)
+                {
+                    num = 0;
                 }
                 else
                 {
@@ -258,7 +217,11 @@ public class FracCalc {
                 num = num/gcd;
                 den = den/gcd;
                 
-                if(whole==0)
+                if(num==den)
+                {
+                    return "Result: "+whole;
+                }
+                else if(whole==0)
                 {
                     return "Result: "+num+"/"+den;
                 }
@@ -325,6 +288,10 @@ public class FracCalc {
                 {
                     return "Result: "+num+"/"+den;
                 }
+                else if(num==den)
+                {
+                    return "Result: "+1;
+                }
                 else
                 {
                     return "Result: "+whole+"_"+num+"/"+den;
@@ -341,20 +308,4 @@ public class FracCalc {
         }
         return GCD(b, a % b);
     }
-
-    		
-    	{
-    	//String result = "";
-    	//result += "In \"SecondOperand\"";
-    	//result += "\n\tWhole number: "+ int2;
-    	//result += "\n\tNumerator: "+ num2;
-    	//result += "\n\tDenominator: "+ den2;
-    
-    	//return result;
-	}
 }
-    	
-    
-    			
-    		
-    
